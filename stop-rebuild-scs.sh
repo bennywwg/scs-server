bd5d6a836053aa731148f8ca795e2e137881e220
Benny Wysong-Grass <bennywwg@gmail.com>
2021-12-21 17:19:44 -0600

    container always restart

--- stop-rebuild-scs.sh
+++ stop-rebuild-scs.sh
@@ -2,4 +2,4 @@ docker container stop scs-server-instance
 docker container rm scs-server-instance
 docker image rm scs-server
 docker build -t scs-server .
-docker run --name scs-server-instance -d -p 8050:8222 scs-server
+docker run --name scs-server-instance -d --restart unless-stopped -p 8050:8222 scs-server
