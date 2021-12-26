f7ca06a2e264b0aa20c64ec2105d1192a652c475
Benny Wysong-Grass <bennywwg@gmail.com>
2021-12-26 12:55:03 -0600

    explainer for stop-rebuild-scs.sh

--- stop-rebuild-scs.sh
+++ stop-rebuild-scs.sh
@@ -1,3 +1,8 @@
+# This script assumes the existence of two docker objects:
+# - scs-server, a docker image
+# - scs-server-instance, a docker container of the scs-server image
+# The script will stop the scs-server-instance, remove it, and remove the scs-server image
+# Then it rebuilds the scs-server image from the working directory and restarts the scs-server-instance
 docker container stop scs-server-instance
 docker container rm scs-server-instance
 docker image rm scs-server
